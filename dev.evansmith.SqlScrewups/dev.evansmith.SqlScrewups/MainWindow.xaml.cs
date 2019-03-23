84d65207bbaf555cdd1a755ebe156ee0d1065ff5
Evan Smith <evansmith1377@gmail.com>
2019-03-23 14:51:36 -0500

    Upload SQL Screwups application code

---
+++ dev.evansmith.SqlScrewups/dev.evansmith.SqlScrewups/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using dev.evansmith.SqlScrewups.ViewModels;
+
+namespace dev.evansmith.SqlScrewups
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ dev.evansmith.SqlScrewups/dev.evansmith.SqlScrewups/ViewModels/LegacyTerritoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
+
+namespace dev.evansmith.SqlScrewups.ViewModels
+{
+    public class LegacyTerritoryViewModel : ViewModelBase
+    {
+        public List<string> TerritoryIds { get; set; } = new List<string> {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        public string TerritoryId { get; set; } = "10";
+
+        public string GenerateSql()
+        {
+            return string.IsNullOrWhiteSpace(TerritoryId)
+                 ? string.Empty
+                 : $"Sales.SalesOrderHeader.CustomerID IN (SELECT Sales.Customer.CustomerID FROM Sales.Customer WHERE Sales.Customer.TerritoryID = {TerritoryId})";
+        }
+    }
+}
